cdc6829d1718395f66f7069e402f73b5c1cdfd60
disasterocks <82711453+Repository13@users.noreply.github.com>
2022-01-23 21:16:28 +0000

    Add files via upload

---
+++ CPShellFiles/commands.bat
@@ -0,0 +1,24 @@
+@echo off
+cls
+echo.
+echo [] - Simplified CMD Commands
+echo {} - Custom command
+echo.
+echo Simplified commands for easy use:
+echo commands - Displays list of commands []
+echo shutdown - Shuts down the computer []
+echo restart - Restarts the computer []
+echo logoff - Logs you out of the current session. []
+echo credits - Shows credits to developers of CPShell {}
+echo sysinfo - Shows computer infomation (OS, ect.) {}
+echo user - Shows your username []
+echo proginfo - Shows version and other info about CPShell {}
+echo control - Opens Control Panel []
+echo calculator - Simple batch calculator {}
+echo CPShell - Go to CPShell Menu {}
+echo usertool - A user tool for creating, deleting and displaying users. {}
+echo newboot - Enables Windows alternative boot animation on windows 11. {}
+echo.
+echo You can also use any standard CMD commands (dir, cd, ect.)
+echo Remember to add bat if you're in certain places!
+echo.
---
+++ CPShellFiles/shutdown.bat
@@ -0,0 +1,7 @@
+@echo off
+cls
+title Shutting down...
+echo It's sad to see you go!
+echo CPShell will shutdown your PC in a few seconds...
+shutdown /s
+pause
